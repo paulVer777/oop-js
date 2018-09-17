c63be4f3396b2a5b251d5e91cb339dea5b737151
p <pawel.gitara@onet.eu>
2018-09-17 10:39:02 +0200

    difficulty bar added

--- main.js
+++ main.js
@@ -1,33 +1,52 @@
-
 class Ball {
 
     constructor() {
 
-        this.speed=30
+        this.speed = 10
         this.positionX = 0
         this.positionY = 0
-        this.moveX = 30
-        this.moveY = 30
+        this.moveX = this.speed
+        this.moveY = this.speed
         this.points = 0
         this.boardX = 0
     }
     collisionCheckerBall() {
 
-        this.positionX > innerWidth - 50 ? this.moveX = -10 : ''
-        this.positionX < 0 ? this.moveX = 10 : ''
+        this.positionX > innerWidth - 50 ? this.moveX = -this.speed : ''
+        this.positionX < 0 ? this.moveX = this.speed : ''
 
-        this.positionY > innerHeight - 50 ? this.moveY = -10 : ''
-        this.positionY < 10 ? this.moveY = 10 : ''
+        this.positionY > innerHeight - 50 ? this.moveY = -this.speed : ''
 
-        /// this statement checks if ball collided with board, if its true it changes the Y direction of ball
+
+        if (this.positionY < 0) {
+
+            const h1 = document.createElement('h1')
+            const button = document.createElement('button')
+
+            button.textContent = 'Try again'
+            button.addEventListener('click', (e) => {
+                location.reload()
+            })
+
+            h1.textContent = 'Game Over'
+            h1.classList.add('farewell')
+
+            window.clearInterval(serial)
+            document.body.appendChild(h1)
+            document.body.appendChild(button)
+
+        }
+
+        /// this statement checks if ball collided with board, if its true it changes the Y direction of the ball
 
         for (let i = 0; i < 200; i++) {
             if (this.positionY < 40 && this.positionX === this.boardX + i) {
-                this.moveY = 10
+                this.moveY = this.speed
             }
         }
         ///////////////////////////////////////////////////////////////////////////////////////////////
     }
+
     collisionCheckerBoard() {
 
         this.boardX < 20 ? this.boardX = 0 : ''
@@ -37,11 +56,18 @@ class Ball {
 
         document.querySelector('.ball').style.left = `${this.positionX += this.moveX}px`
         document.querySelector('.ball').style.bottom = `${this.positionY += this.moveY}px`
-        console.log(this.positionY)
+
     }
     weAreTheChampions() {
-        document.querySelector('.score').textContent = this.points
+        document.querySelector('.score').textContent = `Scores: ${this.points}`
         this.points += 1
     }
+    difficultyLevelUp() {
+        game.speed === 17 ? game.speed = 17 : game.speed++
+        console.log(game.speed)
+    }
+    difficultyLevelDown() {
+        game.speed === 5 ? game.speed = 5 : game.speed--
+        console.log(game.speed)
+    }
 }
-
--- start.js
+++ start.js
@@ -1,7 +1,6 @@
-
 const game = new Ball()
 
-setInterval(() => {
+const serial=setInterval(() => {
 
 
     game.collisionCheckerBall()
@@ -11,12 +10,26 @@ setInterval(() => {
 }, 45)
 
 
+
 window.addEventListener('keypress', (e) => {
 
     game.collisionCheckerBoard()
 
     e.key === 'd' ? document.querySelector('.board').style.left = `${game.boardX+=20}px` : ''
     e.key === 'a' ? document.querySelector('.board').style.left = `${game.boardX+=-20}px` : ''
-    console.log(game.boardX)
 
+
+})
+
+document.querySelector('.up').addEventListener('click', (e) => {
+   
+    game.difficultyLevelUp()
+    document.querySelector('.difficulty-line').style.width = `${game.speed}rem`
+    
+})
+
+document.querySelector('.down').addEventListener('click', (e) => {
+   game.difficultyLevelDown()
+    document.querySelector('.difficulty-line').style.width = `${game.speed}rem`
+    
 })
